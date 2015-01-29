efa8b5fdf246eee4bc296439e1033a0cb5d25b59
Randy Lai <randy.cs.lai@gmail.com>
2015-01-28 19:43:37 -0800

    should close all and check if views are dirty

--- better_close_window.py
+++ better_close_window.py
@@ -2,11 +2,15 @@ import sublime
 import sublime_plugin
 
 class BetterCloseWindow(sublime_plugin.WindowCommand):
-	def run(self):
-		# close the project
-		if int(sublime.version()) >= 3000:
-			self.window.run_command('close_workspace')
-		else:
-			self.window.run_command('close_project')
-		# close the window
-		self.window.run_command('close_window')
+    def run(self):
+        # close all files
+        self.window.run_command('close_all')
+        # check if there are dirty views
+        if any([v.is_dirty() for v in self.window.views()]):
+            return
+        # close the project
+        self.window.run_command('close_project')
+        # close the workspace
+        self.window.run_command('close_workspace')
+        # close the window
+        self.window.run_command('close_window')
