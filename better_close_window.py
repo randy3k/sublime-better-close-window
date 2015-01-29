b8aff8b727fbf359e76bd9ce8633e5bf4acc1601
Randy Lai <randy.cs.lai@gmail.com>
2015-01-28 20:33:08 -0800

    don't close files for project

--- better_close_window.py
+++ better_close_window.py
@@ -3,14 +3,16 @@ import sublime_plugin
 
 class BetterCloseWindow(sublime_plugin.WindowCommand):
     def run(self):
-        # close all files
-        self.window.run_command('close_all')
-        # check if there are dirty views
-        if any([v.is_dirty() for v in self.window.views()]):
-            return
-        # close the project
-        self.window.run_command('close_project')
-        # close the workspace
-        self.window.run_command('close_workspace')
+        # if it is a project, close the project
+        if self.window.project_file_name():
+            # close the workspace
+            self.window.run_command('close_workspace')
+        else:
+            self.window.run_command('close_all')
+            # check if there are dirty views
+            if any([v.is_dirty() for v in self.window.views()]):
+                return
+            # close the sidebar
+            self.window.run_command('close_project')
         # close the window
         self.window.run_command('close_window')
